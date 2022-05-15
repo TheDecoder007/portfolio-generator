4d11103550f93fe87faab3a70cd626bac0d4c309
Ryan Murphy <TheDecoder007@gmail.com>
2022-05-15 16:30:19 -0400

    beginning html

--- app.js
+++ app.js
@@ -1,15 +1,63 @@
-const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
 
-  console.log('================');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+const generatePage = (name, github) => {
+   return`
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Demo</title>
+</head>
+
+
+<body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+</body>
+</html>
+
+`;
 };
 
-printProfileData(profileDataArgs);
+console.log(name, github);
+console.log(generatePage(name, github));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
